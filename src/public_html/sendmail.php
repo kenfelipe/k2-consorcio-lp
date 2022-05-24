61fad4f0ed460b3a6d66fb2bbc9e1702e2cfd115
Felipe Kenzo <felipekenzohamada@gmail.com>
2022-05-24 18:18:59 -0300

    noindex sendmail.php

--- src/public_html/sendmail.php
+++ src/public_html/sendmail.php
@@ -62,6 +62,8 @@
   <title><?= $success ? 'Enviado' : 'Error' ?></title>
   <link rel="icon" type="image/x-icon" href="./assets/img/k2/k2.png">
 
+  <meta name="robots" content="noindex">
+
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
